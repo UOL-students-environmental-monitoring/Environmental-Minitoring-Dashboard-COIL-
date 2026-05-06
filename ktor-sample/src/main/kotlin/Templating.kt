b8fe8c3b0c639397985e14066135eed248059755
Kevin Nguyen <knnguyen265@gmail.com>
2026-05-06 11:09:04 +0100

    linters setup and passed, backend efficiency increased using global variables

--- ktor-sample/src/main/kotlin/Templating.kt
+++ ktor-sample/src/main/kotlin/Templating.kt
@@ -1,32 +1,35 @@
 package com.example
 
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.application.*
-import io.ktor.server.http.content.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.response.respond
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
 import io.ktor.server.thymeleaf.Thymeleaf
 import io.ktor.server.thymeleaf.ThymeleafContent
-import java.sql.Connection
-import java.sql.DriverManager
-import org.jetbrains.exposed.sql.*
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
 
 fun Application.configureTemplating() {
     install(Thymeleaf) {
-        setTemplateResolver(ClassLoaderTemplateResolver().apply {
-            prefix = "templates/thymeleaf/"
-            suffix = ".html"
-            characterEncoding = "utf-8"
-        })
+        setTemplateResolver(
+            ClassLoaderTemplateResolver().apply {
+                prefix = "templates/thymeleaf/"
+                suffix = ".html"
+                characterEncoding = "utf-8"
+            }
+        )
     }
     routing {
         get("/html-thymeleaf") {
             call.respond(ThymeleafContent("index", mapOf("user" to ThymeleafUser(1, "user1"))))
         }
+        get("/trends") {
+            call.respond(ThymeleafContent("trends", mapOf<String, Any>()))
+        }
     }
 }
-data class ThymeleafUser(val id: Int, val name: String)
+
+data class ThymeleafUser(
+    val id: Int,
+    val name: String
+)
